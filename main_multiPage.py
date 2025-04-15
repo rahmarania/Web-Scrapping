2e92ac89f33c5f1c776ccf96e5a57c5978dc739e
Rahma Fairuz Rania <101410489+rahmarania@users.noreply.github.com>
2025-04-15 23:06:26 +0700

    Update main_multiPage.py

--- main_multiPage.py
+++ main_multiPage.py
@@ -11,7 +11,8 @@ page_num = 1
 res = []
 
 while True:
-  url = f"{BASE_URL}?page_num={page_num}"
+  # get the data faster because in one page we collect 100 data
+  url = f"{BASE_URL}?page_num={page_num}&per_page=100"
   print(f"Fetching page {url}...")
   
   response = requests.get(url)
@@ -20,24 +21,31 @@ while True:
     break
   
   soup = BeautifulSoup(response.text,"html.parser")
-  table = soup.find('table',class_ = "table"
+  table = soup.find('table',class_ = "table")
   rows = table.find_all("tr", class_ = "team")
 
   # if the page has no data
   if not rows:
-    print(f"No data found on this page: {page_num}") 
+    print(f"No data found on page {page_num}") 
     break 
   
   for row in rows:
-    team_name = row.find("id", class_ = "name").get_text(strip = True)
-    year = row.find("id", class_ = "year").get_text(strip = True)
-    win_rate = row.find("id", class_ = "pct").get_text(strip = True)
-
-  res.append(["team_name":team_name,
+    team_name = row.find("td", class_ = "name").get_text(strip = True)
+    year = row.find("td", class_ = "year").get_text(strip = True)
+    win_rate = row.find("td", class_ = "pct").get_text(strip = True)
+    
+    res.append({"team_name":team_name,
               "year":year,
-              "win_percentage":win_rate])
+              "win_percentage":win_rate})
   
   page_num += 1
+print(res)
 
-
+# extract
+with open("teams.csv","w",newline = "", encoding = "utf-8") as csvfile:
+  fieldnames = ["team_name","year","win_percentage"]
+  writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+  writer.writeheader()
+  for item in res:
+    writer.writerow(item)
 
